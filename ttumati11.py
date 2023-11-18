d21dca41c95fbe63bdf98925aeb4bcca0af6035b
ttumati1 <ttumati1@csudh.edu>
2023-11-18 06:46:53 +0000

    created a python program

---
+++ ttumati11.py
@@ -0,0 +1,15 @@
+#CSC321
+#Tumati Raviteja Tumati
+
+number = float(input("Enter a number "))
+def square():
+    square = number ** 2
+    return square
+
+print(f"Your number squared is {square()} well done is better than well said.")
+
+
+
+
+
+
